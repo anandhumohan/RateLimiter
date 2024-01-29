cf98efb9d9965b9dc251b124781d7196d3782e26
anandhumohan <anandhumohan5@gmail.com>
2024-01-29 07:17:28 +0000

    final commit

--- src/main/java/com/assessment/anandhu/RateLimiter.java
+++ src/main/java/com/assessment/anandhu/RateLimiter.java
@@ -25,7 +25,6 @@ public class RateLimiter {
             times.poll();
         }
         boolean isAllow = times.size() < rateLimit;
-        ;
 
         if (isAllow) {
             times.offer(currentMillSec);
